e9aa19d859b08b5f79320d3206df2d31ba54fefc
jhrick <hj.rickcp@gmail.com>
2023-10-17 08:34:18 -0300

    add sprite for Player

--- src/main/java/org/example/main/characters/Player.java
+++ src/main/java/org/example/main/characters/Player.java
@@ -1,14 +1,46 @@
 package org.example.main.characters;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Properties;
+
 public class Player {
     private int x;
     private int y;
 
     final private int speed = 3;
 
+    private final BufferedImage playerSprite;
+
     public Player() {
         x = 0;
         y = 0;
+
+        BufferedReader reader;
+
+        Properties properties = new Properties();
+
+        final String configFile = "src/main/resources/config.properties";
+
+        String playerSpritePath;
+
+        try {
+            reader = new BufferedReader(new FileReader(configFile));
+
+            properties.load(reader);
+
+            playerSpritePath = properties.getProperty("player_sprite");
+
+            File imgFile = new File(playerSpritePath);
+
+            playerSprite = ImageIO.read(imgFile);
+
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void moveToLeft() {
@@ -20,11 +52,11 @@ public class Player {
     }
 
     public void moveUp() {
-        y += speed;
+        y -= speed;
     }
 
     public void moveDown() {
-        y -= speed;
+        y += speed;
     }
 
     public int getX() {
@@ -33,5 +65,11 @@ public class Player {
 
     public int getY() {
         return y;
+    }
+
+    public void renderPlayer(Graphics g) {
+        if (playerSprite != null) {
+            g.drawImage(playerSprite, getX(), getY(), null);
+        }
     }
 }
