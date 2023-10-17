acb3aac1f92859ca3d85ef07993ff791ab40aad9
jhrick <hj.rickcp@gmail.com>
2023-10-17 13:59:23 -0300

    add the class Entity

---
+++ src/main/java/org/example/main/entity/Entity.java
@@ -0,0 +1,9 @@
+package org.example.main.entity;
+
+import java.awt.image.BufferedImage;
+
+public class Entity {
+    protected int x, y, speed;
+    protected BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    protected String direction;
+}
